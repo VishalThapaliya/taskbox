c0dc44fb0b47cb7ea30d563d06e5e3b5d51e1f6f
VishalThapaliya <vishal.thapaliya@gmail.com>
2020-02-13 11:29:50 +0100

    taskbox UI

---
+++ storybook-static/precache-manifest.1421954e5f747031d1e0d2e007fb15ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af00df67330f3f2de14b5b330d4d15f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.435021b858948d1ce5ae.bundle.js"
+  },
+  {
+    "url": "runtime~main.435021b858948d1ce5ae.bundle.js"
+  },
+  {
+    "revision": "ffbf385570e3a4d1c0b1",
+    "url": "static/css/main.9ec6dc68.chunk.css"
+  },
+  {
+    "url": "vendors~main.435021b858948d1ce5ae.bundle.js"
+  },
+  {
+    "url": "vendors~main.435021b858948d1ce5ae.bundle.js.LICENSE.txt"
+  }
+]);
